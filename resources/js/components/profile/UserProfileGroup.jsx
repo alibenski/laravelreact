6ad264f6d50da7cdb808c8009101e1e9e9c8ad1d
Allyson Frias <allyson.frias@un.org>
2020-01-10 16:56:49 +0100

    create user profile component init

---
+++ resources/js/components/profile/UserProfileGroup.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export class UserProfileGroup extends Component {
+    render() {
+        return <div></div>;
+    }
+}
+
+export default UserProfileGroup;
