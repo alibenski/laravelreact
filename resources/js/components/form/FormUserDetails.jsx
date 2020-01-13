0505a906421535392e4e6b913533b7bbbf799d31
Allyson Frias <allyson.frias@un.org>
2020-01-13 11:22:50 +0100

    Step 1 User Form

--- resources/js/components/form/FormUserDetails.jsx
+++ resources/js/components/form/FormUserDetails.jsx
@@ -1,7 +1,63 @@
 import React, { Component } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import TextField from "@material-ui/core/TextField";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import MenuIcon from "@material-ui/icons/Menu";
+import Container from "@material-ui/core/Container";
 
 const FormUserDetails = ({ nextStep, handleChange, values, continueNext }) => {
-    return <div>Form User Details</div>;
+    const useStyles = makeStyles(theme => ({
+        root: {
+            flexGrow: 1
+        },
+        menuButton: {
+            marginRight: theme.spacing(2)
+        },
+        title: {
+            flexGrow: 1
+        }
+    }));
+
+    const classes = useStyles();
+    return (
+        <React.Fragment>
+            <AppBar title="Enter User Details" position="static">
+                <Toolbar>
+                    <IconButton
+                        edge="start"
+                        className={classes.menuButton}
+                        color="inherit"
+                        aria-label="menu"
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" className={classes.title}>
+                        Enter User Details
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <Container maxWidth="sm">
+                <TextField
+                    helperText="Enter First Name"
+                    label="First Name"
+                    onChange={handleChange("firstName")}
+                    defaultValue={values.firstName}
+                />
+                <br />
+                <Button
+                    variant="contained"
+                    color="default"
+                    onClick={continueNext}
+                >
+                    Continue
+                </Button>
+            </Container>
+        </React.Fragment>
+    );
 };
 
 export default FormUserDetails;
