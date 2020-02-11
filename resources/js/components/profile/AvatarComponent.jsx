bb7340b0470df83d7c41b296df4858344ad8b2c3
Allyson Frias <allyson.frias@un.org>
2020-02-11 18:15:27 +0100

    additional css props

--- resources/js/components/profile/AvatarComponent.jsx
+++ resources/js/components/profile/AvatarComponent.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Avatar from "@material-ui/core/Avatar";
 import { deepOrange } from "@material-ui/core/colors";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import { Typography } from "@material-ui/core";
 
 const useStyles = makeStyles(theme => ({
     root: {
         display: "flex",
         "& > *": {
             margin: theme.spacing(1)
-        }
+        },
+        minWidth: 275
     },
     orange: {
         color: theme.palette.getContrastText(deepOrange[500]),
@@ -23,21 +27,15 @@ const useStyles = makeStyles(theme => ({
 function AvatarComponent() {
     const classes = useStyles();
     return (
-        <div className={classes.root}>
-            <Avatar
-                alt="Remy Sharp"
-                src="/broken-image.jpg"
-                className={classes.orange}
-            >
-                B
-            </Avatar>
-            <Avatar
-                alt="Remy Sharp"
-                src="/broken-image.jpg"
-                className={(classes.orange, classes.large)}
-            />
-            <Avatar src="/broken-image.jpg" className={classes.large} />
-        </div>
+        <React.Fragment>
+            <Card className={classes.root}>
+                <CardContent>
+                    <Avatar src="/broken-image.jpg" className={classes.large} />
+                    <Typography>User detail component here </Typography>
+                    <Typography>User-Skill component here </Typography>
+                </CardContent>
+            </Card>
+        </React.Fragment>
     );
 }
 
--- resources/js/components/profile/UserProfileGroup.jsx
+++ resources/js/components/profile/UserProfileGroup.jsx
@@ -3,8 +3,10 @@ import AvatarComponent from "./AvatarComponent";
 
 function UserProfileGroup() {
     return (
-        <div>
-            <AvatarComponent />
+        <div className="container mt-4">
+            <div className="row justify-content-center">
+                <AvatarComponent />
+            </div>
         </div>
     );
 }
